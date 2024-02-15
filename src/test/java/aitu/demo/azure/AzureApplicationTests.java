bbe7002aa3de39e2362fa9332bd02c2392a831f8
Nurbolat Amilbek <n.amilbek@efactoring.kz>
2024-02-15 20:02:54 +0600

    Initial commit

---
+++ src/test/java/aitu/demo/azure/AzureApplicationTests.java
@@ -0,0 +1,13 @@
+package aitu.demo.azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
